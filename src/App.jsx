296300d828097256fa693a2e6b75b037a427a07b
Pratik Bhuvad <pbhuvad55@gmail.com>
2024-07-28 22:48:19 +0530

    Modified Navbar Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About';
-import Footer from './components/Footer';
 
 function App() {
 
   return (
     <Router>
-      <div className='container'>
+      <>
         <header>
-          <h4 className='sm:uppercase flex items-center font-extrabold text-sm leading-3 md:text-xl md:leading-5'>
+          <Link to='/'>
+          <h4 className='sm:uppercase flex items-center font-extrabold text-sm leading-3 md:text-lg md:leading-5'>
             <span>TE<br />AM</span> Techno
           </h4>
+          </Link>
           <Navbar />
         </header>
         <main>
@@ -23,10 +24,7 @@ function App() {
             <Route path="/about" element={<About />} />
           </Routes>
         </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
+      </>
     </Router>
   )
 }
